d0629f936a0c45c3d69d880402467295e3e75564
m0711 <mokka.bearp123@gmail.com>
2025-05-22 17:38:28 +0900

    [add]プレイヤーと弾を追加

---
+++ Assets/Furuya/BulletCon.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+/// <summary>
+/// 弾丸を制御するコンポーネント
+/// </summary>
+public class BulletCon : MonoBehaviour
+{
+    /// <summary>弾の生存期間（秒）</summary>
+    [SerializeField] float m_lifeTime = 5f;
+    /// <summary>弾が飛ぶ速さ</summary>
+    [SerializeField] float m_speed = 3f;
+    [SerializeField] int m_bulletDamage = 1;
+
+
+    void Start()
+    {
+
+        // クリックした座標の取得（スクリーン座標からワールド座標に変換）
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // 向きの生成（Z成分の除去と正規化）
+        Vector3 shotForward = Vector3.Scale((mouseWorldPos - transform.position), new Vector3(1, 1, 0)).normalized;
+
+        // 弾に速度を与える
+        GetComponent<Rigidbody2D>().velocity = shotForward * m_speed;
+
+        // 生存期間が経過したら自分自身を破棄する
+        Destroy(this.gameObject, m_lifeTime);
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // tagで識別する
+
+        if (collision.CompareTag("Player") && collision.TryGetComponent(out CharactorBase charactor))
+        {
+            charactor.DamageBehaviour(m_bulletDamage);
+        }
+    }
+}
---
+++ Assets/Furuya/PlayerController.cs
@@ -0,0 +1,168 @@
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+enum BulletType
+{
+    None,
+    Normal,
+    Penetrat,
+}
+
+/// <summary>
+/// ガンマンのキャラクターを操作するコンポーネント
+/// </summary>
+public class PlayerController : CharactorBase
+{
+    /// <summary>左右移動する力</summary>
+    [SerializeField] float m_movePower = 5f;
+    /// <summary>ジャンプする力</summary>
+    [SerializeField] float m_jumpPower = 15f;
+    /// <summary>色の配列</summary>
+    ///[SerializeField] Color[] m_colors = default;
+    /// <summary>弾丸のプレハブ</summary>
+    [SerializeField] GameObject[] m_bulletPrefab = default;
+    /// <summary>銃口の位置を設定するオブジェクト</summary>
+    [SerializeField] Transform m_muzzle = default;
+    /// <summary>入力に応じて左右を反転させるかどうかのフラグ</summary>
+    [SerializeField] bool m_flipX = false;
+
+    [SerializeField] float m_bulletChangeDuration = 5f;
+
+    Rigidbody2D m_rb = default;
+    SpriteRenderer m_sprite = default;
+    /// <summary>m_colors に使う添字</summary>
+    int m_bulletIndex;
+    /// <summary>水平方向の入力値</summary>
+    float m_h;
+    float m_scaleX;
+    /// <summary>最初に出現した座標</summary>
+    Vector3 m_initialPosition;
+
+    float _bulletChangeTime = 0;
+    ///<summary>時間計測のための変数
+    private float time;
+
+
+
+
+
+    BulletType m_bulletType = BulletType.Normal;
+
+    [SerializeField] int _jumpCount = 2;
+    int _currrentJumpCount;
+
+    void Start()
+    {
+        m_rb = GetComponent<Rigidbody2D>();
+        m_sprite = GetComponent<SpriteRenderer>();
+        // 初期位置を覚えておく
+        m_initialPosition = this.transform.position;
+        _currrentJumpCount = _jumpCount;
+        time = 1.0f;　//最初の1回をOK
+    }
+
+    void Update()
+    {
+        // 入力を受け取る
+        m_h = Input.GetAxisRaw("Horizontal");
+
+        // 各種入力を受け取る
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (_currrentJumpCount > 0)
+                //Debug.Log("ここにジャンプする処理を書く。");
+                m_rb.AddForce(new Vector2(0, m_jumpPower), ForceMode2D.Impulse);
+            _currrentJumpCount--;
+        }
+
+        
+        //連射出来ないようにして銃を撃つ
+        time += Time.deltaTime;　//時間を増やす
+        if (time >= 1.0f) //1秒以上経てばOK
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                GameObject bullet = null;
+
+                switch (m_bulletType)
+                {
+                    case BulletType.Normal:
+                        bullet = Instantiate(m_bulletPrefab[0]);
+                        break;
+                    case BulletType.Penetrat:
+                        bullet = Instantiate(m_bulletPrefab[1]);
+                        break;
+                    default:
+                        break;
+                }
+                //Debug.Log("ここに弾を発射する処理を書く。");
+                //bullet.transform.position = transform.position;
+                bullet.transform.position = m_muzzle.position;
+            }
+        time = 0.0f; //時間を初期化
+        }
+
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            //Debug.Log("ここに弾丸を切り替える処理を書く。");
+            m_bulletType = BulletType.Penetrat;
+            _bulletChangeTime = Time.time;
+        }
+
+        if(m_bulletType != BulletType.Normal && Time.time - _bulletChangeTime >= m_bulletChangeDuration)
+        {
+            m_bulletType = BulletType.Normal;
+            Debug.Log(Time.time - _bulletChangeTime);
+        }
+
+       
+
+
+        // 設定に応じて左右を反転させる
+        if (m_flipX)
+        {
+            FlipX(m_h);
+        }
+    }
+
+
+    private void FixedUpdate()
+    {
+        // 力を加えるのは FixedUpdate で行う
+        m_rb.AddForce(Vector2.right * m_h * m_movePower, ForceMode2D.Force);
+    }
+
+    /// <summary>
+    /// 左右を反転させる
+    /// </summary>
+    /// <param name="horizontal">水平方向の入力値</param>
+    void FlipX(float horizontal)
+    {
+        /*
+         * 左を入力されたらキャラクターを左に向ける。
+         * 左右を反転させるには、Transform:Scale:X に -1 を掛ける。
+         * Sprite Renderer の Flip:X を操作しても反転する。
+         * */
+        m_scaleX = this.transform.localScale.x;
+
+        if (horizontal > 0)
+        {
+            this.transform.localScale = new Vector3(Mathf.Abs(this.transform.localScale.x), this.transform.localScale.y, this.transform.localScale.z);
+        }
+        else if (horizontal < 0)
+        {
+            this.transform.localScale = new Vector3(-1 * Mathf.Abs(this.transform.localScale.x), this.transform.localScale.y, this.transform.localScale.z);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            _currrentJumpCount = _jumpCount;
+            Debug.Log("着地");
+        }
+    }
+}
