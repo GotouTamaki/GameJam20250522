b6cf36fdc9463a89fab4a82f315c91c4c10d8c21
ki28ryu <ki28ryu@icloud.com>
2025-06-02 16:43:21 +0900

    つかってないUI を非表示にした。 変数を入れたらスライダーと体力を共有できるようにしてるはずです

--- Assets/Sugimoto/Hpcomtroller.cs
+++ Assets/Sugimoto/Hpcomtroller.cs
@@ -5,17 +5,21 @@ public class PlayerHealth : MonoBehaviour
 {
     private Slider _slider;
     public int _health;
-    private int _maxhealth = 100;
-
+    [SerializeField] CrystalController _controller;
+    private int _maxhealth;
     private void Start()
     {
         _slider = GetComponent<Slider>();//このコンポーネントがついているオブジェクト内のコンポーネントを取得することができる
-        _health = _maxhealth;
+        _maxhealth = _controller.GetCharactorParamater.GetMaxHp;
+        _health = _controller.GetCharactorParamater.GetCurrentHp;
         _slider.maxValue = _maxhealth;
     }
 
     private void Update()
     {
-        _slider.value = _health;
+        if (_health != 0 && _maxhealth != 0)
+        {
+            _slider.value = _health / _maxhealth;
+        }
     }
 }
